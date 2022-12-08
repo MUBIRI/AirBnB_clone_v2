551ddf439b7669fb767118b6787827ef67b6188a
Eninetjanice <janetheni@gmail.com>
2022-12-08 00:51:27 +0000

    updated City model

--- models/state.py
+++ models/state.py
@@ -1,9 +1,24 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+import model
 
 
 class State(BaseModel):
-    """ State class """
-    name = ""
+    """ Defines State class """
+    __tablename__ = 'states'
+
+    if models.storage_type == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', cascade='all, delete, delete_child'
+                        back_populates='state')
+    else:
+        name = ""
+
+        @property
+        def cities(self):
+            cities = models.storage.all('City').values()
+            return [city for city in cities if city.state_id == self.id]
+
