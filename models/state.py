1167833f3f51e650a842261d9e5850b391ede920
Eninetjanice <janetheni@gmail.com>
2022-12-10 11:25:25 +0000

    State update

--- models/state.py
+++ models/state.py
@@ -3,21 +3,27 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-import models
+import models.city import City
 
 
 class State(BaseModel):
     """ Defines State class """
     __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship('City', cascade='all, delete, delete_child'
+                          back_populates='state')
 
-    if models.storage_type == 'db':
-        name = Column(String(128), nullable=False)
-        cities = relationship('City', cascade='all, delete, delete_child'
-                              back_populates='state')
-    else:
-        name = ""
+    @property
+    def cities(self):
+        '''returns the list of City instances with state_id
+            equals the current State.id
+            FileStorage relationship between State and City
+        '''
+        from models import storage
+        related_cities = []
 
-        @property
-        def cities(self):
-            cities = models.storage.all('City').values()
-            return [city for city in cities if city.state_id == self.id]
+        cities = storage.all(City).items()  # gets the entire storage- a dict.
+        for city in cities.values():  # cities.value returns city objects list
+            if city.state_id == self.id:  # if the object.state_id == self.id
+                related_cities.append(city)  # append to the cities list
+        return related_cities
