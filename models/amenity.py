5243c6fc96ff62024f41ab79f13650f28e7e6b97
Eninetjanice <janetheni@gmail.com>
2022-12-10 12:24:01 +0000

    Fixes

--- models/amenity.py
+++ models/amenity.py
@@ -1,20 +1,13 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Table, ForeignKey, Column, String
 from sqlalchemy.orm import relationship
-from models import storage_type
+from models.place import Place
 
 
 class Amenity(BaseModel):
     __tablename__ = 'amenities'
-
-    if storage_type == 'db':
-        name = Column(String(128), nullable=False)
-        place_amenities = Column(ForeignKey('places.id'), nullable=False)
-
-        amenities = relationship('Place', back_populates='amenities')
-
-    else:
-        amenity_id = ""
-        name = ""
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship('Place', secondary=Place.place_amenity,
+                                   back_populates='amenities')
